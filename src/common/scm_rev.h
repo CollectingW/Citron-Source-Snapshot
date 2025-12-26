9a2ce867af1121a68744a620c7b6986a621072be
Collecting <collecting@noreply.localhost>
2025-12-26 02:25:22 +0000

    feat(updater): Improved Updater System w/ Automation & SCM Logic Changes
    Signed-off-by: Collecting <collecting@noreply.localhost>

--- src/common/scm_rev.h
+++ src/common/scm_rev.h
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2014 Citra Emulator Project
+// SPDX-FileCopyrightText: 2025 citron Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
@@ -16,5 +17,10 @@ extern const char g_build_id[];
 extern const char g_title_bar_format_idle[];
 extern const char g_title_bar_format_running[];
 extern const char g_shader_cache_version[];
+
+// Citron Baked Identity
+extern const char g_citron_variant[];
+extern const char g_citron_version[];
+extern const char g_citron_hash[];
 
 } // namespace Common
